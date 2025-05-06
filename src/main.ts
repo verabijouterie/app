17cf6659ef9cfa6b470e86e4c88cc8275758308b
verabijouterie <verabijouterie@gmail.com>
2025-05-06 14:56:31 +0200

    environmet erroc change 3

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode(); // ✅ this disables Angular's dev/debug APIs
 }
+if (environment.production) {
+  Object.defineProperty(window, 'ng', {
+    value: undefined,
+    writable: false
+  });
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
